742b3d5b3b090a2777e68a3923ca10c665046b9f
muhammad maaz <compiletimeterror@gmail.com>
2024-01-06 21:20:21 +0500

    update

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,30 @@
+"use client"
+
+import axios from "axios"
+import Link from "next/link"
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+
 export default function Profile(){
+
+    const router=useRouter()
+
+    const logout=async()=>{
+            try {
+                await axios.get("/api/users/logout");
+                toast.success("login successfull ")
+                router.push("/login")
+            } catch (error:any) {
+                console.log(error.message);
+                toast.error(error.message)
+            }
+    }
 
     return(
         <div className="text-4xl">
             <p>User Profile</p>
-
+            <button className="bg-neutral-200 p-10" onClick={logout}>Logout</button>
         </div>
     )
 }
